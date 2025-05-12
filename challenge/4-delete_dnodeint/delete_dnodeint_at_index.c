ce8f2cea1091f19f236947b881325d0b2e1bed63
Issercio <didi_1374@outlook.fr>
2025-05-12 09:21:51 +0200

    add all the files

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,60 +1,61 @@
-#include "lists.h"
+#include <string.h>
 #include <stdlib.h>
+#include "lists.h"
 
 /**
- * delete_dnodeint_at_index - Deletes a node at a specific index from a doubly
- * linked list.
+ * delete_dnodeint_at_index - Delete a node at a specific index from a list
  *
- * @head: A pointer to the first element of the list.
- * @index: The index of the node to delete.
+ * @head: A pointer to the first element of a list
+ * @index: The index of the node to delete
  *
- * Return: 1 on success, -1 on failure.
+ * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current;
+    dlistint_t *tmp;
     unsigned int i;
 
-    /* Check if the list is empty or the head pointer is NULL */
-    if (head == NULL || *head == NULL)
+    /* If the list is empty, return -1 */
+    if (*head == NULL)
     {
         return (-1);
     }
 
-    current = *head;
-    i = 0;
-
-    /* Traverse the list to find the node to delete */
-    while (current != NULL && i < index)
+    tmp = *head;
+    
+    /* Traverse the list to the node to delete */
+    for (i = 0; tmp != NULL && i < index; i++)
     {
-        current = current->next;
-        i++;
+        tmp = tmp->next;
     }
 
-    /* If the node at the given index doesn't exist */
-    if (current == NULL)
+    /* If index is out of range, return -1 */
+    if (tmp == NULL)
     {
         return (-1);
     }
 
-    /* If the node to delete is the head node */
-    if (current->prev == NULL)
+    /* If deleting the first node */
+    if (tmp == *head)
     {
-        *head = current->next;
-        if (*head)
+        *head = tmp->next;
+        if (*head != NULL)
         {
-            (*head)->prev = NULL;  /* Set the previous pointer of the new head to NULL */
+            (*head)->prev = NULL;
         }
     }
     else
     {
-        current->prev->next = current->next;  /* Adjust the previous node's next pointer */
-        if (current->next)
+        /* Re-link the previous node to the next node */
+        if (tmp->next != NULL)
         {
-            current->next->prev = current->prev;  /* Adjust the next node's prev pointer */
+            tmp->next->prev = tmp->prev;
         }
+        tmp->prev->next = tmp->next;
     }
 
-    free(current);  /* Free the memory of the deleted node */
-    return (1);  /* Return success */
+    /* Free the node */
+    free(tmp);
+
+    return (1);
 }
