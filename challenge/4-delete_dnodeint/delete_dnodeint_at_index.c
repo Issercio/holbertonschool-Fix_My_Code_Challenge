9458e1f387c672194ac927b4104db0228fc70321
Issercio <didi_1374@outlook.fr>
2025-05-12 09:54:37 +0200

    fix

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -2,47 +2,50 @@
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - Deletes a node at a given position.
- * @head: Pointer to the head of the list.
- * @index: The index of the node to be deleted.
- *
- * Return: 1 if it succeeded, -1 if it failed.
+ * delete_dnodeint_at_index - deletes the node at index of a dlistint_t list
+ * @head: double pointer to the head of the list
+ * @index: index of the node to delete
+ * Return: 1 if successful, -1 if failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *temp = *head;
 	unsigned int i = 0;
 
+	/* If the list is empty, return -1 */
 	if (*head == NULL)
-		return (-1); /* If the list is empty, return -1 */
+		return (-1);
 
-	/* Deleting the head node */
+	/* If the node to be deleted is the head node */
 	if (index == 0)
 	{
-		*head = (*head)->next;
+		*head = temp->next;
 		if (*head != NULL)
-			(*head)->prev = NULL; /* Set the prev pointer of the new head to NULL */
+			(*head)->prev = NULL;
 		free(temp);
 		return (1);
 	}
 
-	/* Traverse the list to find the node at the given index */
+	/* Traverse the list to find the node at index */
 	while (temp != NULL && i < index)
 	{
 		temp = temp->next;
 		i++;
 	}
 
-	/* If the index is out of range, return -1 */
+	/* If we reached the end of the list, the index is out of range */
 	if (temp == NULL)
 		return (-1);
 
-	/* Deleting the node at the given index */
-	if (temp->next != NULL)
-		temp->next->prev = temp->prev; /* Connect the next node to the previous one */
+	/* Update the next pointer of the previous node */
 	if (temp->prev != NULL)
-		temp->prev->next = temp->next; /* Connect the previous node to the next one */
+		temp->prev->next = temp->next;
 
+	/* Update the prev pointer of the next node */
+	if (temp->next != NULL)
+		temp->next->prev = temp->prev;
+
+	/* Free the memory of the node */
 	free(temp);
 	return (1);
 }
